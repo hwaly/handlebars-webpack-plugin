03156c57915ef5395944008bf0718573827d0523
Byeonghwa.Yeon <hwalybell@groupidd.com>
2017-10-26 10:47:09 +0900

    업데이트

---
+++ index.js
@@ -0,0 +1,201 @@
+const path = require("path");
+const glob = require("glob");
+const fs = require("fs-extra");
+const chalk = require("chalk");
+const Handlebars = require("handlebars");
+
+
+/**
+ *
+ * @param {mixed} args
+ */
+const log = (...args) => {
+    args.unshift(chalk.gray("HandlebarsPlugin:"));
+    console.log.apply(console, args);
+};
+
+
+
+class HandlebarsPlugin {
+    constructor(options) {
+        if (!options.entryOutput) {
+            log(chalk.red('"entryOutput"을 지정해주세요.'));
+            return;
+        }
+
+        this.root = path.resolve(__dirname, (options.root ? options.root : '../../handlebars'));
+        this.outputRoot = path.resolve(__dirname, (options.outputRoot ? options.outputRoot : '../../../'));
+        this.entries = [];
+        this.outputs = [];
+        this.data = {};
+
+        this.options = {
+            entryOutput: options.entryOutput,
+            data: path.resolve(this.root, '_data/_config.js'),
+            partials: path.resolve(this.root, '_partials/**/*.hbs'),
+            helpers: path.resolve(this.root, '_helpers/**/*.js')
+        };
+
+        this.fileDependencies = [];
+        this.assetsToEmit = {};
+
+        this.build();
+    }
+
+    build() {
+        this
+            .buildEntryOutput()
+            .register('helpers')
+            .register('partials')
+            .updateData()
+    }
+
+    buildEntryOutput() {
+        this.options.entryOutput.forEach((filesPath) => {
+            const entryFiles = this.files(path.resolve(this.root, filesPath[0]));
+            const outputFiles = entryFiles.map(file => {
+                const entryFileName = path.basename(file.replace(this.root, '')).replace(path.extname(file), '');
+
+                return path.resolve(this.outputRoot, filesPath[1].replace("[name]", entryFileName));
+            });
+
+            this.entries = this.entries.concat(entryFiles);
+            this.outputs = this.outputs.concat(outputFiles);
+        });
+
+        return this;
+    }
+
+    updateData(filePath) {
+        this.data = Object.assign(this.data, require(filePath ? filePath : this.options.data));
+
+        return this;
+    }
+
+
+    /**
+     * helpers와 partials에 사용되는 아이디 생성
+     *
+     * @param {string} type
+     * @param {string} path
+     * @returns {string}
+     */
+    getId(type, path) {
+        const id = {
+            helpers: path => this.dashToUpper(path.match(/\/([^/]*).js$/).pop()),
+            partials: path => path.match(/\/([^/]+\/[^/]+)\.[^.]+$/).pop()
+        };
+
+        return id[type](path);
+    }
+
+
+    /**
+     * glob 패턴 파일들
+     *
+     * @param {string} globPath
+     * @returns {array}
+     */
+    files(globPath) {
+        return [].concat(glob.sync(globPath));
+    }
+
+
+    /**
+     * 파일 내용 가져오기
+     *
+     * @param {string} filePath
+     */
+    read(filePath) {
+        return fs.readFileSync(filePath, 'utf8');
+    }
+
+    /**
+     * 케밥 케이스를 카멜 케이스로 변경
+     *
+     * @param {string} text
+     */
+    dashToUpper(text) {
+        return text.replace(/-(\w)/g, (_, letter) => letter.toUpperCase());
+    }
+
+    /**
+     * helpers와 partials에 등록
+     *
+     * @param {string} type
+     */
+    register(type) {
+        this.files(this.options[type])
+            .forEach(path => Handlebars[this.dashToUpper(`register-${type.replace(/s$/, '')}`)](this.getId(type, path), this.read(path)));
+
+        return this;
+    }
+
+    /**
+     *
+     * @param compiler
+     */
+    apply(compiler) {
+        compiler.plugin("make", (compilation, done) => {
+            this.compileAllFile(done);
+        });
+
+        compiler.plugin("emit", (compilation, done) => {
+            compilation.fileDependencies = compilation.fileDependencies.concat(this.fileDependencies);
+            this.emitGeneratedFiles(compilation);
+            done();
+        });
+    }
+
+    /**
+     * 파일 생성
+     *
+     * @param done
+     */
+    compileAllFile(done) {
+        this.entries.forEach((filePath, idx) => {
+            const outputFilepath = this.outputs[idx];
+            const templateContent = this.read(filePath);
+            const templateContentData = path.resolve(this.root, '_data', path.relative(this.root, filePath).replace('.hbs', '.js'));
+            const template = Handlebars.compile(templateContent);
+
+            if (fs.existsSync(templateContentData)) {
+                Object.assign(this.data, require(templateContentData));
+            }
+
+            const result = template(this.data);
+
+            this.fileDependencies.push(filePath);
+
+            fs.outputFileSync(outputFilepath, result, 'utf-8');
+
+            this.registerGeneratedFile(outputFilepath, result);
+        });
+
+        done();
+    }
+
+
+
+
+
+
+    addDependency(...args) {
+        this.fileDependencies.push.apply(this.fileDependencies, args.filter((filename) => filename));
+    }
+    registerGeneratedFile(filepath, content) {
+        this.assetsToEmit[path.basename(filepath)] = {
+            source: () => content,
+            size: () => content.length
+        };
+    }
+    emitGeneratedFiles(compilation) {
+        Object.keys(this.assetsToEmit).forEach((filename) => {
+            compilation.assets[filename] = this.assetsToEmit[filename];
+        });
+    }
+}
+
+HandlebarsPlugin.Handlebars = Handlebars;
+
+module.exports = HandlebarsPlugin;
